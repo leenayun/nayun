b5174cb718299a78c74022114b7611dd11056889
leenayun <48146687+leenayun@users.noreply.github.com>
2020-05-15 17:44:52 +0900

    Updates

---
+++ precache-manifest.72465a61ee3e0fe891c6713fd6bb3432.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12e20f464b4f61586bae3c2cc23593bf",
+    "url": "/nayun/index.html"
+  },
+  {
+    "revision": "8d20f8894c80d31160ac",
+    "url": "/nayun/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8956e06ccd1ddb504981",
+    "url": "/nayun/static/js/2.2ba19105.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nayun/static/js/2.2ba19105.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d20f8894c80d31160ac",
+    "url": "/nayun/static/js/main.b936440f.chunk.js"
+  },
+  {
+    "revision": "7435a4f7d07b9b077663",
+    "url": "/nayun/static/js/runtime-main.0e541084.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/nayun/static/media/logo.5d5d9eef.svg"
+  }
+]);
